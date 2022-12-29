7b73e3d5eaabb002694cc974fc1b918ad2c11c88
Dilnoza Akhrarova <121368213+dilnoza1984@users.noreply.github.com>
2022-12-29 03:15:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+
+  public static void main(String[] args) {
+
+double miles = 100;
+    
+double KIlOMETERS_PER_MILE = 1.609;
+  final double kilometers = miles * KIlOMETERS_PER_MILE;
+  System.out.println(kilometers);  
+  }
+}
